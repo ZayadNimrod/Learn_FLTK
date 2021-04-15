916c88d94271c58543f372120acb6fc529d52f9f
Nimrod Libman <>
2021-04-15 18:13:41 +0100

    Image drawing

--- src/main.rs
+++ src/main.rs
@@ -6,51 +6,30 @@ use fltk::window::*;
 use fltk::group;
 use fltk::frame;
 use std::cell::RefCell;
+use fltk::image;
+
+
 
 fn main() {
+
+
+
+    let mut image = image::PngImage::load("logo.png").unwrap();
     let app = app::App::default();
     app.with_scheme(app::Scheme::Base);
-    let mut window = Window::new(100, 100, 100, 300, "FLTK testing");
-    let mut count:RefCell<u32> = RefCell::new(0);
+    let mut window = Window::new(100, 100, 400, 300, "FLTK testing");
 
-    let mut packing = group::Pack::default().with_size(100,300).center_of(&window);
-    packing.set_type(group::PackType::Vertical);
+    let mut frame = frame::Frame::new(0, 0, 400, 300, "");
 
-    let mut button_inc = Button::default()
-        .with_label("Increment!");
+    frame.draw2(move |f| {
+        image.scale(f.width()/2, f.height()/2, true, true);
+        image.draw(f.x(), f.y(), image.width(), image.height());
+        image.draw(100, 100, image.width(), image.height());
+        image.draw(200, 0, image.width(), image.height());
 
-
-
-
-
-    let mut display = frame::Frame::default()
-        //.center_of(&window)
-        .with_label("0");
-
-
-    let mut button_dec = Button::default()//.with_size(100,50)
-        .with_label("decrement!");
-
-/*
-    button_inc.set_callback(|| {
-        *count.borrow_mut()+=1;
-        display.set_label(&(*count.borrow()).to_string());
+        image.scale(f.width(), f.height(), true, true);
+        image.draw(20,0,image.width(),image.height());
     });
-
-
-    button_dec.set_callback(|| {
-        *count.borrow_mut()-=1;
-        display.set_label(&(*count.borrow()).to_string());
-    });
-
-    button_dec.handle2(|b,event| match event{
-        Event::Enter => {b.set_label("Decrement"); true},
-        Event::Leave => {b.set_label("decrement"); true},
-        _ => false,
-    });
-*/
-    packing.auto_layout();
-    packing.end();
 
 
 
